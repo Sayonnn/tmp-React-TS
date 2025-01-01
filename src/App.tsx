68cddb3ebd6122266ea8c65adb845c6c350679ce
Sayonnn <cascallalaudzion19@gmail.com>
2025-01-01 14:48:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main className ={`w-full h-full p-4 bg-black`}>
+     
+    </main>
+  )
+}
+
+export default App
